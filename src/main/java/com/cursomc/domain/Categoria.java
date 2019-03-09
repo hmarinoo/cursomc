8ae20985c3e3e8a9c4bfd3a9409446cf6811b67b
hmarinoo <hmarinoo@gmail.com>
2019-03-09 19:20:41 -0300

    Database and automatic creation of the database

--- src/main/java/com/cursomc/domain/Categoria.java
+++ src/main/java/com/cursomc/domain/Categoria.java
@@ -4,14 +4,20 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Entity
 public class Categoria implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String Name;
 	
-	private List<?> produtos = new ArrayList<>();
 
 	public Categoria() {
 
