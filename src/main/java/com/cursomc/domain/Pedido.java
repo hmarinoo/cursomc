c922e2eed46144640e02b2f22455ca26c8bbbf37
hmarinoo <hmarinoo@gmail.com>
2019-03-15 23:22:39 -0300

    Pedido, EtadoPagamento e pagamento

---
+++ src/main/java/com/cursomc/domain/Pedido.java
@@ -0,0 +1,118 @@
+package com.cursomc.domain;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+import org.hibernate.annotations.Cascade;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+@Entity
+public class Pedido {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private Date instant;
+
+	@ManyToOne
+	@JoinColumn(name = "cliente_id")
+	private Cliente cliente;
+
+	@ManyToOne
+	@JoinColumn(name = "endereco_de_entraga_id")
+	private Endereco enderecoDeEntrega;
+
+	@OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")
+	private Pagamento pagamento;
+
+	public Pedido() {
+
+	}
+
+	public Pedido(Integer id, Date instant, Cliente cliente, Endereco enderecoDeEntrega) {
+		super();
+		this.id = id;
+		this.instant = instant;
+		this.cliente = cliente;
+		this.enderecoDeEntrega = enderecoDeEntrega;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Date getInstant() {
+		return instant;
+	}
+
+	public void setInstant(Date instant) {
+		this.instant = instant;
+	}
+
+	public Cliente getCli() {
+		return cliente;
+	}
+
+	public void setCli(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Endereco getEnderecoDeEntrega() {
+		return enderecoDeEntrega;
+	}
+
+	public void setEnderecoDeEntrega(Endereco enderecoDeEntrega) {
+		this.enderecoDeEntrega = enderecoDeEntrega;
+	}
+
+	public Pagamento getPagamento() {
+		return pagamento;
+	}
+
+	public void setPagamento(Pagamento pagamento) {
+		this.pagamento = pagamento;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pedido other = (Pedido) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
---
+++ src/main/java/com/cursomc/domain/enums/EstadoPagamento.java
@@ -0,0 +1,37 @@
+package com.cursomc.domain.enums;
+
+public enum EstadoPagamento {
+
+	PENDENTE(1, "Pendente"), 
+	QUITADO(2, "Quitado"),
+	CANCELADO(3, "Cancelado");
+
+	private int cod;
+	private String descricao;
+
+	private EstadoPagamento(int code, String descricao) {
+		this.cod = code;
+		this.descricao = descricao;
+	}
+
+	public int getCod() {
+		return cod;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public static EstadoPagamento toEnum(Integer cod) {
+		if(cod == null) {
+			return null;
+		}
+		for (EstadoPagamento x : EstadoPagamento.values()) {
+			if(cod.equals(x.getCod())) {
+				return x;
+				}
+		}
+		
+		throw new IllegalArgumentException("Estado de pagamento invalido");
+	}
+}
